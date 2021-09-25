76655a102dc65f9e8e9eb3d166e6205e6126f052
berkayg <56236888+berkaygkv@users.noreply.github.com>
2021-09-25 14:02:25 +0300

    Update sql_non_ist_rv.py

--- sql_non_ist_rv.py
+++ sql_non_ist_rv.py
@@ -61,9 +61,9 @@ def get_stores(latitude, longitude, driver):
     load_time = int(time.time() - start)
 
     # Webdriver expression to wait precisely until the Address field becomes visible
-    address_text_xpath = '//div[@data-tooltip="Copy address"]//*[string-length(text()) > 1]'
+    address_text_xpath = '//div[@data-tooltip="Copy address"]//*[string-length(text()) > 2]'
     WebDriverWait(driver, 6).until(EC.text_to_be_present_in_element((By.XPATH, address_text_xpath),''))
-    time.sleep(0.1)
+    time.sleep(1.2)
     
     # Gather the web element object with the corresponding information and get the text value of the object
     address_text = driver.find_element_by_xpath(address_text_xpath).text
